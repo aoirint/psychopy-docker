2fe847bc6414825ae3e0dee72026aa9bce3fa8de
aoirint <aoirint@gmail.com>
2022-10-08 18:42:47 +0900

    Command set: shell script to Makefile (#2)
    shell script to Makefile

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build ./docker \
-    -t aoirint/psychopy-docker \
-    --build-arg BASE_IMAGE=nvidia/opengl:base-ubuntu18.04
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it \
-    --gpus all \
-    -e HOST_UID=$(id -u) \
-    -e HOST_GID=$(id -g) \
-    -e DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    aoirint/psychopy-docker
